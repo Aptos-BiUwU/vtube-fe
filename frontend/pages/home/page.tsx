8d24eea0298260bfca2e37798e248c9d3d889fd7
winprn <minhladz001@gmail.com>
2024-10-30 22:23:12 +0700

    feat: add categories

--- frontend/pages/home/page.tsx
+++ frontend/pages/home/page.tsx
@@ -17,10 +17,43 @@ export default function HomePage() {
         <Fire width="50px" height="50px" />
         <h2 className="font-medium text-4xl font-[FairyMuffin]">Recommended anime live</h2>
       </div>
-      <div className="grid grid-cols-3 flex-grow w-full gap-y-4">
-        {streamers.map((streamer, index) => (
-          <VideoPreview channel={streamer as any} key={index} />
-        ))}
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-3 items-center mb-2">
+          <img
+            src="https://www.toei-animation.com/wp-content/uploads/2019/02/one_piece_product.jpg"
+            className="w-[50px] rounded-full aspect-square"
+          />
+          <h3 className="font-[FairyMuffin] text-3xl primary">One Piece Team!</h3>
+        </div>
+        <div className="grid grid-cols-3 flex-grow w-full gap-y-4">
+          {streamers.slice(0, 5).map((streamer, index) => (
+            <VideoPreview channel={streamer as any} key={index} />
+          ))}
+        </div>
+        <div className="flex gap-3 items-center mb-2">
+          <img
+            src="https://m.media-amazon.com/images/S/pv-target-images/0c9ce4e037546965d6b1f3807e9f8f549a113d32066b2bdb22ada5d179c0d89a.jpg"
+            className="w-[50px] rounded-full aspect-square"
+          />
+          <h3 className="font-[FairyMuffin] text-3xl primary">Naruto Team!</h3>
+        </div>
+        <div className="grid grid-cols-3 flex-grow w-full gap-y-4">
+          {streamers.slice(5, 12).map((streamer, index) => (
+            <VideoPreview channel={streamer as any} key={index} />
+          ))}
+        </div>
+        <div className="flex gap-3 items-center mb-2">
+          <img
+            src="https://m.media-amazon.com/images/I/81Xoz+ob08L.jpg"
+            className="w-[50px] rounded-full aspect-square"
+          />
+          <h3 className="font-[FairyMuffin] text-3xl primary">Other</h3>
+        </div>
+        <div className="grid grid-cols-3 flex-grow w-full gap-y-4">
+          {streamers.slice(12).map((streamer, index) => (
+            <VideoPreview channel={streamer as any} key={index} />
+          ))}
+        </div>
       </div>
     </div>
   );
