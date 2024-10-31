a1bd4845d07a9001406a3e97b3c520f2e228cc3a
winprn <minhladz001@gmail.com>
2024-10-31 22:19:07 +0700

    fix

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -59,7 +59,7 @@ function App() {
           <div className="w-full">
             <h3 className="uppercase text-lg font-bold text-[FairyMuffin] mb-2">Recommended channels</h3>
             <div className="flex flex-col gap-3">
-              {streamers.slice(5, 12).map((streamer, index) => (
+              {streamers.slice(5, 9).map((streamer, index) => (
                 <RecommendChannel key={index} channel={streamer as any} />
               ))}
             </div>
